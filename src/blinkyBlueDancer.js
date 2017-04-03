25c2e108e47de4fc91a2a3e66fb183a53ca8ac57
Hack Reactor Student <student@hackreactor.com>
2017-04-03 16:53:24 -0700

    Pseudoclassical instantiation

---
+++ src/blinkyBlueDancer.js
@@ -0,0 +1,19 @@
+var makeBlinkyBlueDancer = function(top, left, timeBetweenSteps) {
+  var blinkyBlueDancer = makeDancer(top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  var oldStep = blinkyBlueDancer.step;
+
+  blinkyBlueDancer.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    blinkyBlueDancer.$node.toggle();
+  };
+
+  return blinkyBlueDancer;
+};
