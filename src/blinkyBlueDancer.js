414751425f6c7312889bab195c95023941b8ec72
Hack Reactor Student <student@hackreactor.com>
2017-04-03 17:30:55 -0700

    Blinky Dancer working in PSEUDOINST

--- src/blinkyBlueDancer.js
+++ src/blinkyBlueDancer.js
@@ -1,19 +1,30 @@
 var makeBlinkyBlueDancer = function(top, left, timeBetweenSteps) {
-  var blinkyBlueDancer = makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  var oldStep = blinkyBlueDancer.step;
-
-  blinkyBlueDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyBlueDancer.$node.toggle();
-  };
-
-  return blinkyBlueDancer;
+  makeBlinkyDancer.call(this, top, left, timeBetweenSteps);
 };
+
+makeBlinkyBlueDancer.prototype = Object.create(makeBlinkyDancer.prototype);
+makeBlinkyBlueDancer.prototype.constructor = makeBlinkyBlueDancer;
+
+makeBlinkyBlueDancer.prototype.blueify = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  var context = this;
+  setTimeout(function(){
+    context.$node.addClass('blueify');
+  });
+  
+};
+
+// makeBlinkyBlueDancer.prototype.step = function() {
+//   // call the old version of step at the beginning of any call to this new version of step
+//   // toggle() is a jQuery method to show/hide the <span> tag.
+//   // See http://api.jquery.com/category/effects/ for this and
+//   // other effects you can use on a jQuery-wrapped html tag.
+  // var context = this;
+  // setTimeout(function(){
+  //   context.step();
+  // }, 120);
+  // context.$node.toggle();
+// };
--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,5 @@
 // Creates and returns a new dancer object that can step
-var makeDancer = function(top, left, timeBetweenSteps) {
+var dancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
@@ -13,14 +13,13 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 };
 
 
-makeDancer.prototype.step = function() {
+dancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  console.log('hey from step');
   setTimeout(this.step, this.timeBetweenSteps);
 };
 
-makeDancer.prototype.setPosition = function(top, left) {
+dancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
